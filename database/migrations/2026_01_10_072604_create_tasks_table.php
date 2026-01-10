29a2e9c7f6e2c8e99d844f3fb1c8f66b333ec0c8
anissap848-ship-it <anissap848@gmail.com>
2026-01-10 14:33:22 +0700

    setup database, migration tasks, and auth

---
+++ database/migrations/2026_01_10_072604_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');        // Judul Tugas
+            $table->text('description');     // Deskripsi
+            $table->string('priority');     // Prioritas (Sedang/Tinggi)
+            $table->date('deadline');       // Tanggal Deadline
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
